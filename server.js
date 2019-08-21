3c953c10ff1295f7083be95d9d1e4ae8a536305c
TJ <tjames02@hamline.edu>
2019-08-20 22:07:51 -0500

    mongoDB mLab

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -28,7 +28,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/hwScraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/hwScraper";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
